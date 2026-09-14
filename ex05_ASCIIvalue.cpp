2b0d0bc86e18a3c8caaadcc5d0901f977f5f5032
elena-negrea <elenanegrea01@outlook.com>
2026-09-14 20:09:51 +0300

    ASCII file

---
+++ ex05_ASCIIvalue.cpp
@@ -0,0 +1,18 @@
+//Create a program that accepts a single character input from the user.
+// It should then print the decimal integer value that corresponds to that character in the ASCII (American Standard Code for Information Interchange) table.
+#include <iostream>
+
+int main()
+{
+    char ch; 
+    std::cout << "Enter a single character: "<< std::endl;
+    std::cin >> ch; // being single character, has input of single ch
+
+    //Cast the char into an in, wich is the ASCII value of the character
+    int asciiValue = (int)ch;
+
+    std::cout << "The ASCII value of " << ch << " is: " << asciiValue << std::endl;
+
+    return 0;
+
+}
