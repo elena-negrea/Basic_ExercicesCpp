43808438b7b90ff80c23d42dc72861a62f54bc65
elena-negrea <elenanegrea01@outlook.com>
2026-09-14 15:14:59 +0300

    AreaRectangle

---
+++ ex02_AreaRectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Enter the length of the rectangle: ";
+    double length, width;
+    std::cin >> length;
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    
+    return 0;
+}
